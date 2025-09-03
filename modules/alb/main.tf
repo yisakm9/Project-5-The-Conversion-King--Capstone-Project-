b81e70a5214bc0955930fbbe3493b4ca404e52d7
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-03 12:34:17 +0300

    Refine ALB listener configuration with improved comments and maintain temporary fixed response action

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -45,14 +45,16 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
-/*
+
+  /*
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
   }
   */
 
-  # Add a temporary fixed response action
+  # Add a temporary fixed response action used to break dependencies during port fix
+  
   default_action {
     type = "fixed-response"
     fixed_response {
@@ -61,7 +63,7 @@ resource "aws_lb_listener" "http" {
       status_code  = "503"
     }
   }
-
+  
 }
 
 # 4. Crucial Step: Attach the Auto Scaling Group to the Target Group
