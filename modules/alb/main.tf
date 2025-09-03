cc76be528302615f754b3ccbc27b6f0bbb728f2f
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-03 12:17:27 +0300

    Update ALB listener to include a temporary fixed response action for maintenance

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -45,11 +45,23 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
-
+/*
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
   }
+  */
+
+  # Add a temporary fixed response action
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Under Maintenance"
+      status_code  = "503"
+    }
+  }
+
 }
 
 # 4. Crucial Step: Attach the Auto Scaling Group to the Target Group
