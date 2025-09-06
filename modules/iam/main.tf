32fc5d150314315713214e4591779b1d49eabe37
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-06 15:24:51 +0300

    Add SSM policy attachment to EC2 IAM role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -67,3 +67,9 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile-${var.environment}"
   role = aws_iam_role.ec2_role.name
 }
+
+# ADD THIS NEW RESOURCE BLOCK TO ATTACH THE SSM POLICY
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
